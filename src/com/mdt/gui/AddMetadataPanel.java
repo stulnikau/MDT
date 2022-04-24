bbf967461e809db7a23650b5bef4e8508311a45f
stulnikau <pavel.stulnikov@connect.qut.edu.au>
2022-04-24 19:45:40 +1000

    Re-do AddMetadataPanel

--- src/com/mdt/gui/AddMetadataPanel.java
+++ src/com/mdt/gui/AddMetadataPanel.java
@@ -4,36 +4,62 @@ import javax.swing.*;
 import java.awt.*;
 
 public class AddMetadataPanel extends JPanel {
+    private final JPanel metadataPanel;
+    private final JLabel namePrompt;
+    private final JLabel authorPrompt;
+    private final JTextField nameInput;
+    private final JTextField authorInput;
+
+    private void setupLayout() {
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+
+        c.weightx = 0;
+        c.gridy = 0;
+        c.ipadx = 5;
+        metadataPanel.add(namePrompt, c);
+
+        c.gridy = 1;
+        metadataPanel.add(authorPrompt, c);
+
+        c.weightx = 1;
+        c.ipadx = 0;
+        c.gridx = 1;
+        c.gridy = 0;
+        metadataPanel.add(nameInput, c);
+
+        c.gridy = 1;
+        metadataPanel.add(authorInput, c);
+
+        c.gridy = 2;
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.LINE_END;
+        c.weighty = 100.0;
+        c.gridheight = GridBagConstraints.REMAINDER;
+        metadataPanel.add(Box.createGlue(), c);
+    }
+
     public AddMetadataPanel() {
         super(new BorderLayout());
-        JLabel Header = new JLabel("New Maze");
 
-        Header.setBounds(0, 0, 100, 40);
+        JLabel header = new JLabel("New Maze");
+        header.setFont(GUIFrame.HEADING_2);
+        this.add(header, BorderLayout.NORTH);
 
-        add(Header);
-        setSize(400,400);
-        setVisible(true);
-        setLayout(null);
+        metadataPanel = new JPanel(new GridBagLayout());
 
+        namePrompt = new JLabel("Maze Name:");
+        authorPrompt = new JLabel("Author Name:");
 
+        nameInput = new JTextField("New Maze");
+        authorInput = new JTextField("");
 
+        setupLayout();
+        this.add(metadataPanel, BorderLayout.CENTER);
 
-        JTextField input1, input2;
-        input1 = new JTextField("Maze Name");
-        input1.setBounds(50, 100, 150, 40);
-        input2 = new JTextField("Author");
-        input2.setBounds(50, 150, 150, 40);
-        add(input1); add(input2);
-        setSize(400,400);
-        setLayout(null);
-        setVisible(true);
-
-
-
-
-
-
-
+        // Add some padding on the sides
+        this.add(Box.createHorizontalStrut(5), BorderLayout.EAST);
+        this.add(Box.createHorizontalStrut(5), BorderLayout.WEST);
     }
 
 }
