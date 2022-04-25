a9f7ba0c5054ab5eb39ffd7c38ae01e30bace247
Raghav Sharma <Sharmaraghavv382@gmail.com>
2022-04-25 18:22:20 +1000

    Maze Generation Skeleton

---
+++ src/com/mdt/gui/FilePanel.java
@@ -0,0 +1,11 @@
+package com.mdt.gui;
+import javax.swing.*;
+import java.awt.*;
+
+public class FilePanel extends JPanel{
+    public FilePanel() {
+        super(new BorderLayout());
+    }
+    //west
+
+}
