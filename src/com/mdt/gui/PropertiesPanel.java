51f0d41fc579afbc44801294c235c8e0f431e819
Raghav Sharma <Sharmaraghavv382@gmail.com>
2022-04-25 19:20:30 +1000

    Link Maze Generation to rest of the Panels

--- src/com/mdt/gui/PropertiesPanel.java
+++ src/com/mdt/gui/PropertiesPanel.java
@@ -2,11 +2,11 @@ package com.mdt.gui;
 import javax.swing.*;
 import java.awt.*;
 
-public class PropertiesPanel extends JPanel {
+public class PropertiesPanel extends JPanel{
     public PropertiesPanel() {
         super(new BorderLayout());
-
     }
+    //west
 
-    //east
+
 }
